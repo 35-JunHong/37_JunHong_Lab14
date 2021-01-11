4710c2e071746eda09a1fc97c6405835cb13474c
JunHong30 <neojunhong2003@gmail.com>
2021-01-11 20:52:56 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveSpeed = 5.0f;
+
+    public GameObject SphereRed;
+    public GameObject SpherePurple;
+    public GameObject CapsuleYellow;
+    public GameObject CapsuleOrange;
+
+    private int redCharge;
+    private int purpleCharge;
+    private int yellowCharge;
+    private int orangeCharge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+    }
+}
